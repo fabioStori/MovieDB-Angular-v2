9d99785c7b5f70384a0e1e7e70c818d7a13584e5
Fabio Stori <f196631@dac.unicamp.br>
2020-08-05 11:54:09 -0300

    working on movie page

--- src/app/movies/components/movie-page/movie-page.component.ts
+++ src/app/movies/components/movie-page/movie-page.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute, Data } from '@angular/router';
 export class MoviePageComponent implements OnInit {
   movie = {}
   posterUrl = ""
+  collectionPosterUrl = ""
   voteAverage = ""
 
   constructor(private route: ActivatedRoute) { }
@@ -16,6 +17,7 @@ export class MoviePageComponent implements OnInit {
     this.route.data.subscribe((data: Data) => {
       this.movie = data['movie']
       this.posterUrl = `https://image.tmdb.org/t/p/w500/${data['movie']['poster_path']}`
+      this.collectionPosterUrl = `https://image.tmdb.org/t/p/w500/${data['movie']['belongs_to_collection']['backdrop_path']}`
       this.voteAverage = `${data['movie']['vote_average'] * 10}`
       console.log(this.movie)
     })
