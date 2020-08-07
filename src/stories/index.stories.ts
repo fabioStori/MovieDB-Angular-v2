ac77992e67890a9fb6f58186daf773b078021520
Fabio Stori <f196631@dac.unicamp.br>
2020-08-07 11:41:22 -0300

    working with storybook

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -1,78 +1,42 @@
 import { moduleMetadata, storiesOf } from '@storybook/angular';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import { HeaderComponent } from '../app/layout/components/header/header.component';
 import { FooterComponent } from '../app/layout/components/footer/footer.component';
 import { MovieInformationComponent } from '../app/movies/components/movie-information/movie-information.component';
 import { RouterModule, ActivatedRoute } from '@angular/router';
-import { CommonModule, APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF } from '@angular/common';
 import 'bootstrap/dist/css/bootstrap.css';
+import { MoviePageComponent } from '../app/movies/components/movie-page/movie-page.component';
 
-import { Welcome, Button } from '@storybook/angular/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => ({
-  component: Welcome,
-  props: {},
-}));
-
-storiesOf('Button', module)
-  .add('with text', () => ({
-    component: Button,
-    props: {
-      text: 'Hello Button',
-    },
-  }))
-  .add(
-    'with some emoji',
-    () => ({
-      component: Button,
-      props: {
-        text: '😀 😎 👍 💯',
+const RouterModuleLayoutImport = [
+  RouterModule.forRoot(
+    [
+      {
+        path: '',
+        loadChildren: () =>
+          import('../app/layout/layout.module').then((m) => m.LayoutModule),
       },
-    }),
-    { notes: 'My notes on a button with emojis' }
-  )
-  .add(
-    'with some emoji and action',
-    () => ({
-      component: Button,
-      props: {
-        text: '😀 😎 👍 💯',
-        onClick: action('This was clicked OMG'),
+    ],
+    { useHash: true }
+  ),
+];
+const RouterModuleMoviesImport = [
+  RouterModule.forRoot(
+    [
+      {
+        path: '',
+        loadChildren: () =>
+          import('../app/movies/movies.module').then((m) => m.MoviesModule),
       },
-    }),
-    { notes: 'My notes on a button with emojis' }
-  );
+    ],
+    { useHash: true }
+  ),
+];
 
-storiesOf('Another Button', module).add(
-  'button with link to another story',
-  () => ({
-    component: Button,
-    props: {
-      text: 'Go to Welcome Story',
-      onClick: linkTo('Welcome'),
-    },
-  })
-);
 storiesOf('Header', module)
   .addDecorator(
     moduleMetadata({
       declarations: [HeaderComponent],
-      imports: [
-        CommonModule,
-        RouterModule.forRoot(
-          [
-            {
-              path: '',
-              loadChildren: () =>
-                import('../app/layout/layout.module').then(
-                  (m) => m.LayoutModule
-                ),
-            },
-          ],
-          { useHash: true }
-        ),
-      ],
+      imports: [...RouterModuleLayoutImport],
       providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
     })
   )
@@ -94,8 +58,6 @@ storiesOf('Footer', module)
   .addDecorator(
     moduleMetadata({
       declarations: [FooterComponent],
-      imports: [CommonModule],
-      providers: [],
     })
   )
   .add('default', () => {
@@ -110,14 +72,29 @@ storiesOf('MovieInformationComponent', module)
   .addDecorator(
     moduleMetadata({
       declarations: [MovieInformationComponent],
-      imports: [CommonModule],
-      providers: [ActivatedRoute],
+      imports: [...RouterModuleMoviesImport],
     })
   )
   .add('default', () => {
     return {
       template: `
         <app-movie-information></app-movie-information>
+    `,
+      props: {},
+    };
+  });
+storiesOf('MoviePageComponent', module)
+  .addDecorator(
+    moduleMetadata({
+      declarations: [MoviePageComponent],
+      imports: [...RouterModuleMoviesImport],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    })
+  )
+  .add('default', () => {
+    return {
+      template: `
+        <app-movie-page></app-movie-page>
     `,
       props: {},
     };
