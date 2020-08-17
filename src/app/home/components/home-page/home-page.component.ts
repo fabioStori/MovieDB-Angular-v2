802ebb727622afa1366a0d317e5baf8350105ad0
Fabio Stori <fstori@vmware.com>
2020-08-17 14:27:14 -0300

    adding pagination to home page

--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute, Data } from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
   movies = [];
+  currentSlide = 0;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -15,5 +16,52 @@ export class HomePageComponent implements OnInit {
     this.route.data.subscribe((data: Data) => {
       this.movies = data.movie.results;
     });
+  }
+
+  isNextSlide(slideIndex: number): boolean {
+    // const columnSize = 3;
+    // const slideColumns = 2;
+    // return (this.currentSlide + slideColumns) * columnSize <= slideIndex;
+    return true;
+  }
+
+  isCurrentSlide(slideIndex: number): boolean {
+    switch (this.currentSlide) {
+      case 0: {
+        if (slideIndex <= 3) return true;
+        else return false;
+      }
+      case 1: {
+        if (slideIndex > 3 && slideIndex <= 7) return true;
+        else return false;
+      }
+      case 2: {
+        if (slideIndex > 7 && slideIndex <= 11) return true;
+        else return false;
+      }
+      case 3: {
+        if (slideIndex > 11 && slideIndex <= 15) return true;
+        else return false;
+      }
+    }
+    // const columnSize = 3;
+    // const slideColumns = 2;
+    // return (
+    //   slideIndex >= this.currentSlide * columnSize &&
+    //   slideIndex < (this.currentSlide + slideColumns) * columnSize
+    // );
+  }
+
+  isBackSlide(slideIndex: number): boolean {
+    // const columnSize = 3;
+    // return this.currentSlide * columnSize > slideIndex;
+    return true;
+  }
+
+  back() {
+    this.currentSlide--;
+  }
+  next() {
+    this.currentSlide++;
   }
 }
